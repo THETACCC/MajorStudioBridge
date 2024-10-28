e9748eb0a1ef54ef6830e6c39583a47ba22727ed
THETACCC <qingyuanfan03@gmail.com>
2024-10-27 20:35:13 -0400

    Init push

---
+++ Assets/GameLogic/BloodManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodManager : MonoBehaviour
+{
+
+    public int bloodCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bloodCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
